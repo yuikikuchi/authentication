8792cedbf2968e18ef30560295b1d9b32f7a56e5
Yui Kikuchi <yui.kikuchi.mgmt@gmail.com>
2025-01-30 20:59:35 +0900

    first commit

---
+++ auth01/src/main/java/com/example/auth01/Auth01Application.java
@@ -0,0 +1,13 @@
+package com.example.auth01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth01Application.class, args);
+	}
+
+}
